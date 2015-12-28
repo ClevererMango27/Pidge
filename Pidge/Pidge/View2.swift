1718c42d3f62f44afdc4cf78c1f078fdb8596430
ClevererMango27 <cleverermango27@gmail.com>
2015-12-27 22:26:55 -0500

    ayy

---
+++ Pidge/Pidge/View2.swift
@@ -0,0 +1,35 @@
+//
+//  View2.swift
+//  Pidge
+//
+//  Created by Brendon Duan Ho on 12/27/15.
+//  Copyright © 2015 Pidge. All rights reserved.
+//
+
+import UIKit
+
+class View2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pidge/Pidge/ViewController.swift
+++ Pidge/Pidge/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollview: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    var V1 : View1 = View1(nibName: "View1", bundle: nil)
+    var V2 : View2 = View2(nibName: "view2", bundle: nil)
+    
+    self.addChildViewController(V1)
+    self.scrollView.addSubView(V1.view)
+    
+    V1.didMoveToParentController(self)
+    
+    self.addChildViewController(V2)
+    self.scrollView.addSubView(V2.view)
+    V2.didMoveToParentViewController(self)
+    
+    var V2Frame : CGRect = V2.view.frame
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

2003b2100c004b60c1cd9212e731c05d9bef070f
ClevererMango27 <cleverermango27@gmail.com>
2015-12-31 22:52:09 -0500

    I hate XCode 7.2. Thanks Tim Cook

---
+++ Groupsnap/Groupsnap/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Groupsnap
+//
+//  Created by Brendon Ho on 12/31/15.
+//  Copyright © 2015 Pidge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //View 1
+        
+        //View 2
+        
+        //Camera View or View 3
+        
+        //Additional Code
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
